97b62cdbcf3e0924497c04c1c8f9f0783f00703c
lauriberman <lauriberman@gmail.com>
2022-04-28 16:45:01 -0300

    Cambios

--- Proyecto final.py
+++ Proyecto final.py
@@ -7,6 +7,8 @@
 import csv 
 
 from collections import OrderedDict 
+from datetime import datetime
+from email import header
 
 # Funciones
 
@@ -22,6 +24,7 @@ def read_csv():
     lista = []
     lista_prendas = []
     lista_talles = []
+    
 
     # Imprimir lista de marcas para que el usuario ingrese en cual quiere comprar: 
 
@@ -43,6 +46,8 @@ def read_csv():
         
 
     # Si lo está, imprimir las prendas que tenga esa marca en el local
+    # separar funcion? 
+
   
     for prenda in data: # Acceder a cada prenda de la lista data
         if prenda['MARCA'] == marca_ingresada:
@@ -60,6 +65,9 @@ def read_csv():
         print('Excelente! Aquí los detalles de la prenda que elegiste:\n')
     else: 
         print('La prenda ingresada no se encuentra disponible.')
+
+        # volver a preguntar?
+        # separar funcion? 
 
     for prenda in data:
         if prenda['PRENDA'] == prenda_elegida:
@@ -85,19 +93,81 @@ def read_csv():
         print('Lo sentimos mucho. La prenda que elegiste no tiene ese talle.')
 
 
+    # separar funcion?
+    # Cantidad que va a llevar? 
+
+    cantidad_prenda = int(input('Qué cantidad de tu prenda querés llevar?\n'))
+
+    for prenda in data:
+        if prenda['PRENDA'] == prenda_elegida and prenda['TALLE'] == talle_prenda:
+            for k, v in prenda.items():
+                if k == 'STOCK' and int(v) >= cantidad_prenda:
+                    print('Excelente! Contamos con stock suficiente.')
     
+    if int(v)  < cantidad_prenda:
+        print('No tenemos stock disponible para tu prenda.')
+
+        # preguntar si quiere comprar otro producto
+                    
+    for prenda in data:
+        if prenda['PRENDA'] == prenda_elegida and prenda['TALLE'] == talle_prenda:
+            for k, v in prenda.items():
+                if k == 'PRECIO':
+                    costo_unidad = v
+                   
+    print('El costo de la prenda por unidad es:',costo_unidad)
+    
+    producto = print('Resumen de compra: MARCA:', marca_ingresada,
+     'PRENDA:', prenda_elegida,
+     'PRECIO UNIDAD:', costo_unidad, 
+     'TALLE:', talle_prenda,
+     'CANTIDAD:', cantidad_prenda)
+    
+
+def compra_csv():
+
+
+    with open('compra_final.csv', 'w') as csvfile:
+        header = ['YEY HOUSE', 'MARCA', 'PRENDA', 'PRECIO', 'TALLE', 'CANTIDAD']
+        writer = csv.DictWriter(csvfile,fieldnames=header)
+
+        writer.writeheader()
+        writer.writerow({'YEY HOUSE': datetime.today().strftime('%d-%m-%Y')})
+      
+    
+
+#def imprimir_producto(producto):
+ # nombre = producto.get('nombre')
+  #precio = producto.get('precio')
+  #print(f'producto {nombre} precio {precio}')
+
+#if __name__ == '__main__':
+  # Leo un csv
+ # with open('archivo.csv', 'r') as csvfile:
+  #  data = list(csv.DictReader(csvfile))
+  
+  # Agarro como producto al primer elemento de data  
+  #nuevo_producto = data[0]
+  # Ejemplo, nuevo_producto ahora podría ser {'nombre': 'Vaso', 'precio': 100}
+  #imprimir_producto(nuevo_producto)
+
+
 
 if __name__ == '__main__':
     print('Bienvenidxs a Yey House!')
     read_csv()
 
+
     respuesta_usuario = input('Te interesa comprar otro producto?\n').upper()
     
     if respuesta_usuario == 'SI':
         read_csv()
+    else:
+        compra_csv()
 
         # Generar un archivo csv nuevo con la confirmacion del/de los producto(s) 
         # comprados por el usuario
 
 
 
+# datetime.today().strftime('%d-%m-%Y')
